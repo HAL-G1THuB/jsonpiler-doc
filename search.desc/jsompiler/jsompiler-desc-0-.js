a99b8562b6a715dd930bcb45b20f05210bf202d4
HAL-G1THuB <tan.koro.ring@gmail.com>
2025-04-17 15:53:25 +0900

    Jsompiler -> Jsonpiler

--- search.desc/jsompiler/jsompiler-desc-0-.js
+++
@@ -1,1 +0,0 @@
-searchState.loadedDescShard("jsompiler", 0, "(main.rs)\nParser and compiler.\nBuilds the assembly code from the parsed JSON. This …\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nRuns the Jsonpiler, compiling and executing a JSON-based …")
--- search.desc/jsonpiler/jsonpiler-desc-0-.js
+++ search.desc/jsonpiler/jsonpiler-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("jsonpiler", 0, "(main.rs)")
+searchState.loadedDescShard("jsonpiler", 0, "(main.rs)\nParser and compiler.\nBuilds the assembly code from the parsed JSON. This …\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nRuns the Jsonpiler, compiling and executing a JSON-based …")
