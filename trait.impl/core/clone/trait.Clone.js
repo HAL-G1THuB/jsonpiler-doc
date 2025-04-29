41b35515a49f570b4e78d141c229b57020f5cd7a
HAL-G1THuB <tan.koro.ring@gmail.com>
2025-04-29 21:15:47 +0900

    Version 2.2

--- trait.impl/core/clone/trait.Clone.js
+++ trait.impl/core/clone/trait.Clone.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"jsonpiler/enum.Align.html\" title=\"enum jsonpiler::Align\">Align</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>"]]],["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>"]]]]);
+    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"jsonpiler/enum.Align.html\" title=\"enum jsonpiler::Align\">Align</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[517,276]}
+//{"start":57,"fragment_lengths":[517]}
--- trait.impl/core/marker/trait.Freeze.js
+++ trait.impl/core/marker/trait.Freeze.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"enum\" href=\"jsonpiler/enum.Align.html\" title=\"enum jsonpiler::Align\">Align</a>",1,["jsonpiler::Align"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]],["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]]]);
+    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"enum\" href=\"jsonpiler/enum.Align.html\" title=\"enum jsonpiler::Align\">Align</a>",1,["jsonpiler::Align"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[577,308]}
+//{"start":57,"fragment_lengths":[577]}
