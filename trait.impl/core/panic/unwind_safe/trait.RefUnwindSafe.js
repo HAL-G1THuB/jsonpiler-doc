c8941fe466f50e889147aeb0f7b3ce773c777e73
HAL-G1THuB <tan.koro.ring@gmail.com>
2025-04-29 17:41:47 +0900

    Version 2.2

--- trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
+++ trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]]]);
+    var implementors = Object.fromEntries([["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"enum\" href=\"jsonpiler/enum.Align.html\" title=\"enum jsonpiler::Align\">Align</a>",1,["jsonpiler::Align"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]],["jsonpiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"jsonpiler/struct.Jsonpiler.html\" title=\"struct jsonpiler::Jsonpiler\">Jsonpiler</a>",1,["jsonpiler::Jsonpiler"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[351]}
+//{"start":57,"fragment_lengths":[665,352]}
