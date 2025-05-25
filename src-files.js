5e5ca15c81adfcb66eddf4c9be97d8f94675f666
HAL-G1THuB <tan.koro.ring@gmail.com>
2025-05-25 14:55:34 +0900

    Version 0.3.3

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["jsonpiler",["",[],["builtin.rs","compiler.rs","func_info.rs","json.rs","lib.rs","object.rs","parser.rs","utility.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["jsonpiler",["",[],["bind.rs","builtin.rs","compiler.rs","json.rs","lib.rs","name.rs","object.rs","parser.rs","scope_info.rs","utility.rs"]]],["jsonpiler",["",[],["builtin.rs","compiler.rs","func_info.rs","json.rs","lib.rs","object.rs","parser.rs","utility.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[121]}
+//{"start":36,"fragment_lengths":[142,122]}
